733b63bc6d101ba02ed6801083d5c1c9a4fd19fa
Dhruv Parthasarathy <dhruvp@mit.edu>
2018-12-31 18:53:06 -0800

    remove vendor also

--- vendor/cache/ruby/2.5.0/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/dhruv/Documents/math-blog/vendor/cache/ruby/2.5.0/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
--- vendor/cache/ruby/2.5.0/gems/jekyll-katex-0.3.0/lib/jekyll-katex.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'jekyll'
-require 'jekyll/tags/katex'
-require 'jekyll/tags/katex_math_mode'
-
-Liquid::Template.register_tag('katex', Jekyll::Tags::Katex)
-Liquid::Template.register_tag('katexmm', Jekyll::Tags::KatexMathMode)
